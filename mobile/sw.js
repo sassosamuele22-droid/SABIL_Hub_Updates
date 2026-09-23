05d09a4c4f660e7577bedf9c3ab4920f68a2746a
sassosamuele22-droid <sassosamuele22@gmail.com>
2026-09-23 21:48:05 +0200

    Update sw.js

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE='sabil-gr-mobile-v02';
+const CACHE='sabil-gr-mobile-v03';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
